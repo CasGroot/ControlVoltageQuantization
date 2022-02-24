a61051f73eec5fcca8dd8e854e8b8511769d6b59
CasGroot <cas.groot.12@hotmail.com>
2022-02-24 13:26:53 +0100

    Update for new model architecture and plots
    Support for 2 to 1 architecture
    Script now plots output for all quantized formats

--- find_accuracy.py
+++ find_accuracy.py
@@ -2,13 +2,14 @@ import os
 import torch
 from brainspy.utils.io import load_configs
 from bspytasks.models.default_ring import DefaultCustomModel
+from bspytasks.models.Architecture21 import Architecture21
 from brainspy.algorithms.modules.performance.accuracy import get_accuracy
 from brainspy.utils.manager import get_criterion
 from bspytasks.ring.tasks.classifier import plot_perceptron
 import matplotlib.pyplot as plt
 
 #path to reproducibility file
-path= 'C:/Users/CasGr/Documents/github/brainspy-tasks/tmp/ring/searcher_0.5gap_2022_02_14_200312/reproducibility/'
+path= 'C:/Users/CasGr/Documents/github/brainspy-tasks/tmp/ring/searcher_0.5gap_2022_02_23_125316/reproducibility/'
 #path to file where plots should be saved
 plot_dir= 'C:/Users/CasGr/Documents/github/plots'
 
@@ -22,37 +23,40 @@ quantized_control_voltages = torch.load(os.path.join(path, "quantized_control_vo
 
 #creating a new model instance and setting quantized control voltages
 new_model_instance = DefaultCustomModel(configs['processor'])
-new_model_instance.set_control_voltages(quantized_control_voltages['fixed4'])
+# new_model_instance = Architecture21(configs['processor'])
 
-#finding predictions using quantized control voltages
-for param in new_model_instance.parameters():
-    param.requires_grad = False 
-prediction_train = new_model_instance(results['train_results']['inputs'])
-prediction_test = new_model_instance(results['test_results']['inputs'])
+plot_perceptron(results['test_results']['accuracy'], save_dir=plot_dir, name="test")
+for key in quantized_control_voltages:
+    new_model_instance.set_control_voltages(quantized_control_voltages[key])
 
-#training a perceptron to find a suitable threshold
-accuracy_dict_training = get_accuracy(prediction_train, results['train_results']['targets'], configs['accuracy'], node = None)
+    #finding predictions using quantized control voltages
+    for param in new_model_instance.parameters():
+        param.requires_grad = False 
+    prediction_train = new_model_instance(results['train_results']['inputs'])
+    prediction_test = new_model_instance(results['test_results']['inputs'])
 
-#finding the accuracy of the model on test data
-accuracy_dict_test = get_accuracy(prediction_test, results['test_results']['targets'], configs['accuracy'], node = accuracy_dict_training['node'])
+    #training a perceptron to find a suitable threshold
+    accuracy_dict_training = get_accuracy(prediction_train, results['train_results']['targets'], configs['accuracy'], node = None)
 
-#plotting perceptron 
-plot_perceptron(accuracy_dict_test, save_dir=plot_dir, name="quantized")
-plot_perceptron(results['test_results']['accuracy'], save_dir=plot_dir)
+    #finding the accuracy of the model on test data
+    accuracy_dict_test = get_accuracy(prediction_test, results['test_results']['targets'], configs['accuracy'], node = accuracy_dict_training['node'])
 
-#plotting output of original and quantized model
-plt.figure()
-plt.plot(results['test_results']['best_output'], c='blue', label='original')
-plt.plot(prediction_test, c='r', label='quantized')
-plt.title('output (nA)')
-plt.legend()
-plt.savefig(os.path.join(plot_dir, "output.jpg"))
+    #plotting perceptron 
+    plot_perceptron(accuracy_dict_test, save_dir=plot_dir, name="quantized" + key)
 
-#calculating different losses for output of the model
-fisherloss = loss_fn(prediction_test, results['test_results']['targets'])
-difference = results['test_results']['best_output'] - prediction_test
-mse = torch.sum(difference**2)/difference.size()[0]
-rmse = torch.sqrt(mse)
-print('mse: {}'.format(mse))
-print('rmse: {}'.format(rmse))
-print('fisher: {}'.format(fisherloss[0]))
+    #plotting output of original and quantized model
+    plt.figure()
+    plt.plot(results['test_results']['best_output'], c='blue', label='original')
+    plt.plot(prediction_test, c='r', label=key)
+    plt.title('output (nA)')
+    plt.legend()
+    plt.savefig(os.path.join(plot_dir, "output" + key + ".jpg"))
+
+    #calculating different losses for output of the model
+    fisherloss = loss_fn(prediction_test, results['test_results']['targets'])
+    difference = results['test_results']['best_output'] - prediction_test
+    mse = torch.sum(difference**2)/difference.size()[0]
+    rmse = torch.sqrt(mse)
+    print('mse: {}'.format(mse))
+    print('rmse: {}'.format(rmse))
+    print('fisher: {}'.format(fisherloss[0]))
