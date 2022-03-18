2d1070e769e97f3e72e2ff4ec26a6033054f1863
CasGroot <cas.groot.12@hotmail.com>
2022-03-18 09:46:10 +0100

    Cleaned up find_accuracy.py code

--- find_accuracy.py
+++ find_accuracy.py
@@ -8,12 +8,10 @@ from brainspy.utils.manager import get_criterion
 from bspytasks.ring.tasks.classifier import plot_perceptron
 import matplotlib.pyplot as plt
 import yaml
-import torchvision
-from torch.utils.tensorboard import SummaryWriter
 
 
 def plot_output(original, quantized, key):
-    # plotting output of original and quantized model
+    # plotting output of original and quantized model in one plot
     plt.figure()
     plt.plot(original, c='blue', label='original')
     plt.plot(quantized, c='r', label=key)
@@ -23,6 +21,7 @@ def plot_output(original, quantized, key):
 
 
 def subplot_output(original, quantized, key):
+    # plotting output of original and quantized model in subplots
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.suptitle('output (nA)')
     ax1.plot(original)
@@ -33,6 +32,7 @@ def subplot_output(original, quantized, key):
 
 
 def absmse_append(original, quantized, key):
+    # append dictionary of mse without offset
     absdiff = original - quantized
     if (torch.sum(absdiff**2)/absdiff.size()[0] is not None):
         absmsedict['absolute mse ' + key] = torch.sum(absdiff**2)/absdiff.size()[0]
@@ -41,6 +41,7 @@ def absmse_append(original, quantized, key):
 
 
 def relmse_append(original, quantized, key):
+    # append dictionary of mse with offset
     meandiff = torch.mean(quantized) - torch.mean(original)
     shiftedquantized = quantized - meandiff
     reldiff = original - shiftedquantized
@@ -49,6 +50,7 @@ def relmse_append(original, quantized, key):
 
 
 def rmse_append(original, quantized, key):
+    # append dictionary of rmse
     absdiff = original - quantized
     if (torch.sqrt(torch.sum(absdiff**2)/absdiff.size()[0]) is not None):
         rmsedict['rmse ' + key] = torch.sqrt(torch.sum(absdiff**2)/absdiff.size()[0])
@@ -57,6 +59,7 @@ def rmse_append(original, quantized, key):
 
 
 def mean_append(mean1, mean2, std, i):
+    # append dictionary of mean and std
     mean1dict[i] = mean1
     mean2dict[i] = mean2
     stddict[i] = std
@@ -68,11 +71,11 @@ def plot_accuracyvsbits(accuracyarray):
     plt.scatter((36 - torch.linspace(4, 32, 29)), accuracyarray)
     plt.xlabel('number of bits')
     plt.ylabel('accuracy')
-    log.add_figure('accuracy', fig)
     fig.savefig(os.path.join(plot_dir, "accuracy_vs_bits.jpg"))
 
 
 def save_dicts():
+    # save all dictionaries
     with open(os.path.join(plot_dir, 'losses.yaml'), 'w') as file:
         yaml.dump(str(absmsedict), file)
         yaml.dump(str(relmsedict), file)
@@ -81,7 +84,7 @@ def save_dicts():
         yaml.dump(str(mean2dict), file)
         yaml.dump(str(stddict), file)
     dicts = {
-            'absmse': absmsedict,
+            'absmsedict': absmsedict,
             'relmsedict': relmsedict,
             'rmsedict': rmsedict,
             'mean1dict': mean1dict,
@@ -91,89 +94,114 @@ def save_dicts():
     torch.save(dicts, os.path.join(plot_dir, 'info.pickle'))
 
 
-# path to reproducibility file
-path = r'C:\Users\CasGr\Documents\uni\BachelorOpdracht\Results\electodes_6_2\searcher_0.1gap_2022_02_28_161510_21\reproducibility'
-# path to file where plots should be saved
-plot_dir = 'C:/Users/CasGr/Documents/github/plots'
+if __name__ == "__main__":
 
-# loading in necessary files
-loss_fn = get_criterion("fisher")
-configs = load_configs(os.path.join(path, "configs.yaml"))
-results = torch.load(os.path.join(path, "results.pickle"))
+    # path to reproducibility file
+    path = r'C:\Users\CasGr\Documents\github\brainspy-tasks\tmp\ring\ring_classification_gap_0.1_2022_03_17_160342\reproducibility'
+    # path to file where plots should be saved
+    plot_dir = 'C:/Users/CasGr/Documents/github/plots'
 
-# model_state_dict = torch.load(os.path.join(path, "model.pt"))
-training_data = torch.load(os.path.join(path, "training_data.pickle"))
-quantized_control_voltages = torch.load(os.path.join(path, "quantized_control_voltages.pickle"))
+    # loading in necessary files
+    loss_fn = get_criterion("fisher")
+    configs = load_configs(os.path.join(path, "configs.yaml"))
+    results = torch.load(os.path.join(path, "results.pickle"))
 
-# quantized_control_voltages2 = {}
-# quantized_control_voltages2['original'] = quantized_control_voltages['original']
+    # model_state_dict = torch.load(os.path.join(path, "model.pt"))
+    training_data = torch.load(os.path.join(path, "training_data.pickle"))
+    quantized_control_voltages = torch.load(os.path.join(path, "quantized_control_voltages.pickle"))
 
-# creating a new model instance and setting quantized control voltages
-# new_model_instance = DefaultCustomModel(configs['processor'])
-# model = 'DCM'
-new_model_instance = Architecture21(configs['processor'])
-model = 'Arch21'
-new_model_instance.load_state_dict(training_data['model_state_dict'])
+    quantized_control_voltages2 = {}
+    
+    # quantized_control_voltages2['fixed32frac30'] = quantized_control_voltages['fixed32frac30']
+    quantized_control_voltages2['fixed4frac2'] = quantized_control_voltages['fixed4frac2']
 
-# create summarywriter
-log = SummaryWriter(os.path.join(plot_dir, "log"))
+    # creating a new model instance and setting quantized control voltages
+    # new_model_instance = DefaultCustomModel(configs['processor'])
+    # model = 'DCM'
+    new_model_instance = Architecture21(configs['processor'])
+    model = 'Arch21'
 
-# plot non-quantized accuracy
-plot_perceptron(results['test_results']['accuracy'], save_dir=plot_dir, name="test")
+    new_model_instance.load_state_dict(training_data['model_state_dict'])
+    # plot non-quantized accuracy
+    plot_perceptron(results['test_results']['accuracy'], save_dir=plot_dir, name="test")
 
-# initialize dictionaries
-absmsedict = {}
-relmsedict = {}
-rmsedict = {}
-mean1dict = {}
-mean2dict = {}
-stddict = {}
-accuracyarray = results['test_results']['accuracy']['accuracy_value'].view(1)
-i = 32
+    # initialize dictionaries
+    absmsedict = {}
+    relmsedict = {}
+    rmsedict = {}
+    mean1dict = {}
+    mean2dict = {}
+    stddict = {}
+    accuracyarray = results['test_results']['accuracy']['accuracy_value'].view(1)
 
-for key in quantized_control_voltages:
-    new_model_instance.set_control_voltages(quantized_control_voltages[key])
-    # finding predictions using quantized control voltages
-    for param in new_model_instance.parameters():
-        param.requires_grad = False 
+    i = 32
 
-    # adds mean if there is a batchnorm layer
-    if (model == 'Arch21'):
-        prediction_train = new_model_instance(results['train_results']['inputs'])
-        prediction_test = new_model_instance(results['test_results']['inputs'])
-        mean = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].mean(0)
-        std = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].std()
-        log.add_scalar('mean dnpu1', mean[0], i)
-        log.add_scalar('mean dnpu2', mean[1], i)
-        log.add_scalar('standard deviation', std, i)
-        mean_append(mean[0], mean[1], std, i)
-    elif (model == 'DCM'):
-        prediction_train = new_model_instance(results['train_results']['inputs'])
-        prediction_test = new_model_instance(results['test_results']['inputs'])
+    for key in quantized_control_voltages2:
+        new_model_instance.set_control_voltages(quantized_control_voltages[key])
 
-    # training a perceptron to find a suitable threshold
-    accuracy_dict_training = get_accuracy(prediction_train, results['train_results']['targets'], configs['accuracy'], node=None)
+        # finding predictions using quantized control voltages
+        for param in new_model_instance.parameters():
+            param.requires_grad = False 
+        if (model == 'Arch21'):
+            prediction_test = new_model_instance(results['test_results']['inputs'])
+            new_model_instance.get_running_mean()
+            mean = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].mean(0)
+            std = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].std()
+            # new_model_instance.set_running_mean()
+            # print(new_model_instance.get_running_mean())
+        # adds mean if there is a batchnorm layer
+        if (model == 'Arch21'):
+            prediction_train = new_model_instance(results['train_results']['inputs'])
+            prediction_test = new_model_instance(results['test_results']['inputs'])
+            mean = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].mean(0)
+            std = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].std()
+            mean_append(mean[0], mean[1], std, i)
+        elif (model == 'DCM'):
+            prediction_train = new_model_instance(results['train_results']['inputs'])
+            prediction_test = new_model_instance(results['test_results']['inputs'])
 
-    # finding the accuracy of the model on test data
-    accuracy_dict_test = get_accuracy(prediction_test, results['test_results']['targets'], configs['accuracy'], node = accuracy_dict_training['node'])
+        # training a perceptron to find a suitable threshold
+        # accuracy_dict_training = get_accuracy(prediction_train, results['train_results']['targets'], configs['accuracy'], node=None)
 
-    # plotting perceptron 
-    plot_perceptron(accuracy_dict_test, save_dir=plot_dir, name="quant_" + key)
+        # finding the accuracy of the model on test data
+        # accuracy_dict_test = get_accuracy(prediction_test, results['test_results']['targets'], configs['accuracy'], node = accuracy_dict_training['node'])
 
-    # plotting outpout
-    subplot_output(results['test_results']['best_output'], prediction_test, key)
-    plot_output(results['test_results']['best_output'], prediction_test, key)
+        # plotting perceptron 
+        # plot_perceptron(accuracy_dict_test, save_dir=plot_dir, name="quant_" + key)
 
-    # calculating different losses for output of the model
-    # fisherloss = loss_fn(prediction_test, results['test_results']['targets'])
-    absmse_append(results['test_results']['best_output'], prediction_test, key)
-    relmse_append(results['test_results']['best_output'], prediction_test, key)
-    rmse_append(results['test_results']['best_output'], prediction_test, key)
+        # plotting outpout
+        subplot_output(results['test_results']['best_output'], prediction_test, key)
+        plot_output(results['test_results']['best_output'], prediction_test, key)
 
-    # adding accuracy values to array
-    accuracyarray = torch.cat((accuracyarray, accuracy_dict_test['accuracy_value'].view(1)))
-    save_dicts()
-    i -= 1
+        # calculating different losses for output of the model
+        # fisherloss = loss_fn(prediction_test, results['test_results']['targets'])
+        absmse_append(results['test_results']['best_output'], prediction_test, key)
+        relmse_append(results['test_results']['best_output'], prediction_test, key)
+        rmse_append(results['test_results']['best_output'], prediction_test, key)
 
-plot_accuracyvsbits(accuracyarray[1:30])
-log.close()
+        # adding accuracy values to array
+        # accuracyarray = torch.cat((accuracyarray, accuracy_dict_test['accuracy_value'].view(1)))
+        save_dicts()
+        i -= 1
+    
+    # obtaining mean and std before batchnorm
+    mean = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].mean(0)
+    std = new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].std(0)
+
+
+    print(new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].mean(0))
+    print(new_model_instance.dnpu_l1.get_logged_variables()['d_batch_norm_output'].mean(0))
+    # updating mean and std of batchnorm
+    new_model_instance.set_running_mean(torch.FloatTensor(2).uniform_(10000,100001))
+    new_model_instance.set_running_var(std**2)
+    # new_model_instance.set_running_mean(mean)
+    # new_model_instance.set_running_var(std**2)
+    # print(new_model_instance.get_running_mean())
+    
+    # plot running mean/std corrected output
+    prediction_test2 = new_model_instance(results['test_results']['inputs'])
+    
+    print(new_model_instance.dnpu_l1.get_logged_variables()['c_dnpu_output'].mean(0))
+    print(new_model_instance.dnpu_l1.get_logged_variables()['d_batch_norm_output'].mean(0))
+    subplot_output(results['test_results']['best_output'], prediction_test2, 'fixedbatchnorm')
+    # plot_accuracyvsbits(accuracyarray[1:30])
