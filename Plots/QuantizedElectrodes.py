9e2e3e2546dd400bc6cdfeb3b52dc863cb291e71
CasGroot <cas.groot.12@hotmail.com>
2022-04-13 11:42:15 +0200

    Cleaned up code

--- Plots/QuantizedElectrodes.py
+++ Plots/QuantizedElectrodes.py
@@ -4,22 +4,59 @@ import numpy as np
 from brainspy.utils.io import load_configs
 import matplotlib.pyplot as plt
 
-path = r'C:\Users\CasGr\Documents\uni\BachelorOpdracht\inputspace\-1_1'
-plot_dir = r'C:\Users\CasGr\Documents\github\plots'
+path = r'C:\Users\CasGr\Documents\uni\BachelorOpdracht\inputspace\-1_1\NewModel'
+plot_dir = r'C:\Users\CasGr\Documents\uni\BachelorOpdracht\Results\Plots_1_003_nA\Configurations'
 
 # Obtain outputs
-outputs = torch.load(os.path.join(plot_dir, 'AllConfDict.pickle'))
-output_original = outputs['quantized_input_space_8']['no_electrodes']
+outputs = torch.load(os.path.join(path, 'AllConfDict8bits.pickle'))
+output_original = outputs['no_electrodes']
+
 # Initialize necessary variables
 loss = torch.nn.MSELoss()
-key = 'quantized_input_space_8'
 array = np.array([])
 
 # Obtain error for every configuration
 for i in range(0, 7):
-    array = np.append(array, np.sqrt(loss(output_original, list(outputs[key]['one_electrode'].values())[i]).detach().numpy()))
+    array = np.append(array, np.sqrt(loss(output_original, list(outputs['one_electrode'].values())[i]).detach().numpy()))
+
+# Used to order for 1 quantized electrode
+order = np.array([6,4,2,0,1,3,5])
+orderedarray = np.ones(7)
+
+# Order configurations
+for i in range(0, 7):
+    orderedarray[order[i]] = array[i]
 
 # Plot error
-plt.scatter(np.linspace(0,6,7), array)
-plt.xticks(np.linspace(0, 6, 7), np.array([0,4,5,6,2,3,1]))
+fig = plt.figure()
+# Plot for 1 quantized electrode
+# plt.scatter(np.linspace(0,6,7), orderedarray)
+# plt.ylabel('RMSE (nA)')
+# plt.xlabel('Quantized Electrodes')
+# plt.show()
+# fig.savefig(os.path.join(plot_dir, 'RMSEvsQuantizedElectrodeordered.png'))
+
+array2 = np.array([])
+for i in range(0, 21):    
+    array2 = np.append(array2, np.sqrt(loss(output_original, list(outputs['two_electrodes'].values())[i]).detach().numpy()))
+
+# Plot for 2 quantized electrodes
+# plt.scatter(np.linspace(0,20,21), array2)
+# plt.xticks(np.linspace(0, 20, 21), list(outputs['two_electrodes'].keys()))
+# plt.ylabel('RMSE (nA)')
+# plt.xlabel('Quantized Electrodes')
+# plt.show()
+
+array3 = np.array([])
+for i in range(0, 35):    
+    # if '3' in list(outputs[key]['two_electrodes'].keys())[i]:
+    array3 = np.append(array3, np.sqrt(loss(output_original, list(outputs['three_electrodes'].values())[i]).detach().numpy()))
+
+# Plot for 3 quantized electrodes
+plt.scatter(np.linspace(0,34,35), array3)
+plt.xticks(np.linspace(0,34,35), list(outputs['three_electrodes'].keys()))
+plt.xlabel('Quantized Electrodes')
+plt.ylabel('REMSE (nA)')
 plt.show()
+
+fig.savefig(os.path.join(plot_dir, 'RMSEvsQuantizedElectrode.png'))
